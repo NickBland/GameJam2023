402d65518d3eda77619e3229a5e1461863517275
Nick Bland <nick.bland@nickbland.dev>
2023-10-30 14:43:01 +1000

    add in change game state switch statements

--- js/sketch.js
+++ js/sketch.js
@@ -1,10 +1,73 @@
 "use strict";
 
+
+
+
+let gameState = {
+    loading: true,
+    mainMenu: false,
+    game: false,
+    endScreen: false,
+}
+
+function changeGameState(type) {
+    gameState.loading = gameState.mainMenu = gameState.game = gameState.endScreen = false;
+    switch(type) {
+        case "loading":  
+            loading = true;
+            break;
+        case "mainMenu":
+            gameState.mainMenu = true;
+            break;
+        case "game":
+            gameState.game = true;
+            break;
+        case "endScreen":
+            gameState.endScreen = true;
+            break;
+    }
+}
+
+function drawLoadingScreen() {
+    background("red");
+}
+
+function drawMainMenuScreen() {
+    background("blue");
+}
+
+function drawGameScreen() {
+    background("green");
+}
+
+function drawEndScreen() {
+    background("black");
+}
+
+function preload() {
+
+}
+
 function setup() {
     new Canvas(400,400);
     //Works
 }
 
 function draw() {
-    background(120,120,120);
+    switch(true) {
+        case gameState.loading:
+            drawLoadingScreen();
+            break;
+        case gameState.mainMenu:
+            drawMainMenuScreen();
+            break;
+        case gameState.game:
+            drawGameScreen();
+            break;
+        case gameState.endScreen:
+            drawEndScreen();
+            break;
+    }   
+
+
 }
